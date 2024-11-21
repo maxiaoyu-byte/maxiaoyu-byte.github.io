55f81d99cf59b9dd22c65b95274d9457a7a80b5d
maxiaoyu-byte <19833381279@163.com>
2024-11-21 21:19:02 +0800

    Site updated: 2024-11-21 21:19:01

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732194705938";
+const VERSION = "1732195135977";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
